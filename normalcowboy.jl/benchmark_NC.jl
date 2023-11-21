9a11579d385b6db7e2625e2fb609fa7d99444855
jdbrunner <jdbrunner@lanl.gov>
2023-11-21 08:13:44 -0700

    added benchmarking functions to julia and python API

---
+++ normalcowboy.jl/benchmark_NC.jl
@@ -0,0 +1,90 @@
+using ArgParse
+using CSV,Tables
+
+include("makeTable.jl")
+include("solveProgram.jl")
+
+
+#######        How do mean/variance of diagonal chunks change with chunk size
+
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--lasso", "-l"
+        help = "LASSO penalty strength."
+        default = 1.0
+        arg_type = Float64
+    "--chunksize", "-c"
+        help = "Size of sub-blocks to fit. 0 for regular GLASSO"
+        default = 0
+        arg_type = Int64
+    "--name","-n"
+        help = "Name of output network"
+        default = "network"
+    "--zero_method","-z"
+        help = "How zeros are handled - options dirichlet/add_pseudocounts/robust"
+        default = "dirichlet"
+    "--table_type","-t"
+        help = "Type of table given - options counts/relative"
+        default = "counts"
+    "--return_type","-r"
+        help = "Type of table returned - covariance or precision (inverse covariance) - options covariance/precision"
+        default = "covariance"
+    "otutables"
+        help = "list of OTU tables used to create network."
+        required = true
+        nargs='+'
+end
+parsed_args = parse_args(ARGS, s)
+
+# const env = Gurobi.Env()
+
+
+function normalCowboyMain()
+
+    dsets = Dict()
+    for j in parsed_args["otutables"]
+        dsets[j] = import_table(j)
+    end
+
+    cov_table,clr_table = make_covtable(dsets,method=parsed_args["zero_method"],table_type=parsed_args["table_type"])
+    
+    if parsed_args["chunksize"] !=0
+        covar_matrix = solve_Chunky(cov_table,parsed_args["lasso"],chnksize = parsed_args["chunksize"])
+        if lowercase(parsed_args["return_type"]) == "precision"
+            precision_m = inv(covar_matrix)
+        end
+    else
+        precision_m = solve_withSDP(cov_table,parsed_args["lasso"])
+        if lowercase(parsed_args["return_type"]) == "covariance"
+            covar_matrix = inv(precision_m)
+        end
+    end
+
+    nm = parsed_args["name"]
+
+    cov_table_tab = Tables.table(hcat(clr_table[:otus],cov_table))
+    CSV.write("$nm.cov_table.csv", cov_table_tab, header = vcat([""],clr_table[:otus]))
+
+    clr_tab = Tables.table(hcat(clr_table[:otus],clr_table[:otuTable]))
+    CSV.write("$nm.clr_table.csv",  clr_tab, header = vcat([""],clr_table[:samples]))
+
+    if lowercase(parsed_args["return_type"]) == "covariance"
+        estimate_covtable = Tables.table(hcat(clr_table[:otus],covar_matrix))
+        CSV.write("$nm.csv",  estimate_covtable, header = vcat([""],clr_table[:otus]))
+    else
+        estimate_precision = Tables.table(hcat(clr_table[:otus],precision_m))
+        CSV.write("$nm.csv",  estimate_precision, header = vcat([""],clr_table[:otus]))
+    end
+
+    # marg_table = Tables.table(hcat(clr_table[:otus],off_diag))
+    # CSV.write("$nm.marginals.csv",marg_table,header = vcat([""],clr_table[:otus]))
+
+end
+
+function main()
+    B = @benchmark normalCowboyMain()
+    println(B.times/10^9)#for seconds
+    println(B.memory/(10^6))#for KB
+end
+
+main()
--- python_api/python_api.py
+++ python_api/python_api.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sys
 import os
 import pkgutil
+import numpy as np
 
 def glasso(otu_tables,lam = 0.03,normtype = "dirichlet",chunkyness = 0,name = "glasso",table_type="counts",return_precision = False):
 
@@ -64,3 +65,64 @@ def glasso(otu_tables,lam = 0.03,normtype = "dirichlet",chunkyness = 0,name = "g
     glasso_fit = pd.read_csv("{}.csv".format(name),index_col = 0)
     return glasso_fit
 
+def benchmarkglasso(otu_tables,lam = 0.03,normtype = "dirichlet",chunkyness = 0,name = "glasso",table_type="counts",return_precision = False):
+
+    """Python function to run normalCowboy from script.
+
+    :param otu_tables: set of otu tables (if multiple data types) or single table. (read counts or relative abundance)
+    :type otu_tables: dict(pd.DataFrame) or pd.DataFrame
+
+    :param lam: LASSO penalty. Default 0.03
+    :type lam: float
+
+    :param normtype: type of table normalization. Options - dirichlet/add_pseudocounts/robust. Default "dirichlet"
+    :type normtype: str
+
+    :param chunkyness: Size of sub-blocks to fit. 0 for regular GLASSO. Default 0
+    :type chunkyness: int
+
+    :param name: name to use for temp files passed to julia. Default "glasso"
+    :type name: str
+
+    :param table_type: type of OTU table(s) provided. Options counts/relative. Default "counts"
+    :type table_type: str
+
+    :param return_precision:  Whether to return the precision matrix instead of the covariance matrix. Default False
+    :type return_precision: bool
+
+    :return: estimated covariance matrix or precision matrix
+    :rtype: pd.DataFrame
+    """
+
+    tot_taxa = 0
+
+    if return_precision:
+        rtype = "precision"
+    else:
+        rtype = "covariance"
+
+    current = pkgutil.get_loader("normalCowboy").get_filename()
+    current = os.path.dirname(os.path.abspath(current))
+
+    julpath = os.path.join(current,"normalcowboy.jl","benchmark_NC.jl")
+
+
+    if isinstance(otu_tables,dict):
+        csvnames = []
+        for ky,tab in otu_tables.items():
+            tot_taxa += len(tab)
+            tab.to_csv("otutab_{}.csv".format(ky))
+            csvnames += ["otutab_{}.csv".format(ky)]
+        command = "julia " + julpath +" "+ " ".join(csvnames) + " -l={} -c={} -z={} -n={} -t={} -r={}".format(lam,chunkyness,normtype,name,table_type,rtype)
+    
+    else:
+        tot_taxa = len(otu_tables)
+        otu_tables.to_csv("otutab.csv")
+        command = "julia " + julpath + " otutab.csv" + " -l={} -c={} -z={} -n={} -t={} -r={}".format(lam,chunkyness,normtype,name,table_type,rtype)
+
+    output = subprocess.run(command,shell = True, capture_output=True)
+    glasso_fit = pd.read_csv("{}.csv".format(name),index_col = 0)
+    timestrb,memstrb = output.stdout.splitlines()
+    times = [float(n) for n in str(timestrb).replace("[","").replace("]","").replace("b","").replace("'","").split(", ")]
+    mem = float(memstrb)
+    return glasso_fit,np.mean(times),mem,times
